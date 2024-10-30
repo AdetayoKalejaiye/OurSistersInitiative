c2bf4ea9645563497632fdc69f825840b10f9012
AdetayoKalejaiye <adetayo.kale@gmail.com>
2024-10-30 01:10:05 -0500

    Update config.py

--- women_support_forum/config.py
+++ women_support_forum/config.py
@@ -8,7 +8,7 @@ class Config:
     BASE_DIR = os.path.abspath(os.path.dirname(__file__))
     
     # Database configuration
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'instance', 'forum.db')
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'forum.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     
 
